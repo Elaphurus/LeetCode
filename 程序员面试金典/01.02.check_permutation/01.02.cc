011be1af9a61ee72697f7cc6d8244bf514783ddd
Mingzhe Hu <humingzhework@163.com>
2022-01-11 13:54:46 +0800

    Add 01.02.check_permutation

---
+++ 程序员面试金典/01.02.check_permutation/01.02.cc
@@ -0,0 +1,36 @@
+#include <string>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkPermutation(string s1, string s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        unordered_map<char, int> dic;
+        for (char c : s1) {
+            dic[c] += 1;
+        }
+        for (char c : s2) {
+            dic[c] -= 1;
+        }
+        for (auto kv : dic) {
+            // cout << kv.first << ":" << kv.second << endl;
+            if (kv.second != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main() {
+    Solution s = Solution();
+    cout << s.checkPermutation("abc", "bac") << endl;
+    return 0;
+}
